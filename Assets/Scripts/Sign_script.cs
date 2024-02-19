3e390bca9c28fda9a102519187f2eb2392175b7b
Mem222222 <matthewb40000@gmail.com>
2024-02-18 20:23:36 -0500

    added sign functionaly

--- Assets/Scripts/Sign_script.cs
+++ Assets/Scripts/Sign_script.cs
@@ -7,23 +7,24 @@ public class Sign_script : MonoBehaviour
 {  //Put On Player
     string Sign;
     public TMP_Text Sign_text;
+    public GameObject target;
     //You'll need to manually set the textbox from the prefab
     void Start()
     {
 
     }
     private void OnTriggerStay2D(Collider2D collision)
-    {
+    { Animal animal = target.GetComponent<Animal>();
        
         //put animal class for habitat
         if (collision.gameObject.tag == "Player")
-        
-        { Sign = "Press E";
+           
+        {   Sign = "Press E or R to interact with the " + animal;
             if (Input.GetKey(KeyCode.E))
             {
                 Sign = "";
                 //Interact() function goes here!!!
-                
+                animal.Eat();
             }
         }
     }
